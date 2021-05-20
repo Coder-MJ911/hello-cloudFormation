6a49547d4f9c6a1a0e0e9977bd28dc3a80d1d555
Jiajie <jiajie.ma@thoughtworks.com>
2021-05-20 10:04:50 +0800

    create bucket

---
+++ src/main/java/S3/PutObject.java
@@ -0,0 +1,34 @@
+package S3;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+public class PutObject {
+
+    private final String bucketName;
+    private final String keyName;
+    final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.AP_SOUTHEAST_2).build();
+
+    public PutObject(String bucket_name, String key_name) {
+        this.bucketName = bucket_name;
+        this.keyName = key_name;
+    }
+
+    public void save(String data_content) {
+        System.out.format("Uploading %s to S3 bucket %s...\n", data_content, bucketName);
+        try {
+            s3.putObject(bucketName, keyName, data_content);
+        } catch (
+                AmazonServiceException e) {
+            System.err.println(e.getErrorMessage());
+            System.exit(1);
+        }
+        System.out.println("S3 saving done!");
+    }
+
+    public String get(String bucketName, String key) {
+        return s3.getObjectAsString(bucketName, key);
+    }
+}
